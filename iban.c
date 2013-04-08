bd3da0eb9c04b5cfcfcb49543165bc567a420a6f
Isaak Osipovich Dunayevsky <dailylama@gmail.com>
2013-04-08 14:26:10 +0300

    country iban length

--- iban.c
+++ iban.c
@@ -4,16 +4,15 @@
 #include	<stdlib.h>
 #include	<errno.h>
 
-#define	buf_size 28		/* max buf length */
+#define	buf_size 64		/* max buf length */
 #define	input_len 128		/* input length	*/
+
+char IBAN['Z' + 1]['Z' + 1];
 
 char
 check_iban(const char *const input, int len) {
-  char c, buf[buf_size + 8], iso[buf_size + 8];
+  char c, buf[buf_size], *result;
   int i, j;
-
-//  strncat(buf, iso, 8);
-//  iban.c|15| warning: will never be executed
 
   for (i = j = 0; i < len; i++, j++) {
     if (j > buf_size - 1) {
@@ -25,44 +24,65 @@ check_iban(const char *const input, int len) {
     if (isblank(c)) {
       j--;
     }
-
     else {
 
       // alpha & numeric
       if (isalnum(c)) {
-
-        // deny letters after position 2
-        if (isalpha(c) && j > 1) {
-          return 1;
-        }
-
-        // deny numbers at position 1 and 2
-        if (isdigit(c) && j < 2) {
-          return 1;
-        }
-
         buf[j] = c;
-
       }
-
-      // if not alpha or numeric
       else {
         return 1;
       }
+
     } // not spaces
   } // for-loop
 
   len = j;
   buf[len] = '\0';
 
-  // len must be less than buf_size for strncat
-  if (len < 8 || len > 20) {
+  // format: letter, letter, digit, digit ...
+  //  index:    0       1      2      3
+
+  // deny letters at position 2 and 3
+  if (isalpha(buf[2]) || isalpha(buf[3])) {
+    return 1;
+  }
+
+  // deny numbers at position 0 and 1
+  if (isdigit(buf[0]) || isdigit(buf[1])) {
+    return 1;
+  }
+
+  // first number shouldn't be any of: 00, 01, 99
+  if (
+      (buf[2] == '0' && buf[3] == '0') ||
+      (buf[2] == '0' && buf[3] == '1') ||
+      (buf[2] == '9' && buf[3] == '9')
+     ) {
+    return 1;
+  }
+
+  // Norway has shortest IBAN, which is 15, longest is 32
+  if (len < 15 || len > 32) {
+    return 1;
+  }
+
+  // check if country is registered
+
+  // check iban length
+  i = buf[0];
+  j = buf[1];
+
+  if (IBAN[i][j] != len) {
+    fprintf(stderr, "IBAN '%s' length is %d, but for country %c%c it is %d\n",
+        buf, len, buf[0], buf[1], IBAN[i][j]);
     return 1;
   }
 
   // take 'DE87' from "DE87 1234 5678 1234 5678"
   // convert 'DE' to '1314' append '131487'
-  i = snprintf(iso, 8, "%d%d%c%c", 
+  snprintf(buf, buf_size - 1, "%s%d%d%c%c", 
+      buf,
       buf[0] - 55, buf[1] - 55,	// 'DE' country code
       buf[2], buf[3]			// 87
       );
@@ -72,29 +92,30 @@ check_iban(const char *const input, int len) {
     return 1;
   }
 
-  strncat(buf, iso, 8);
-
   // shift buf
-  char *result = buf + 4;
+  result = buf + 4;
 
   // input: 'DE87 1111 2222 3333 8888 '
   //output: '1111222233338888131487'
-
   printf("output: '%s'\n", result);
+
+//  printf("output: '%s'\n", result);
 
 #if 1
   char *p;
 
   errno = 0;
 
+  long long unsigned int n =
   strtoull(result, &p, 10);
+//  strtoull(result, &p, 10);
 
   if (p != '\0' || errno != 0) {
     fprintf(stderr, "Error: Number is too big ?\n");
     perror("Error");
   }
 
-  //  if (n % 97 == 1) return 0;
+  if (n % 97 == 1) return 0;
 #endif
 
   return 1;
@@ -102,6 +123,30 @@ check_iban(const char *const input, int len) {
 
 int
 main(int argc, char *argv[]) {
+
+  IBAN['A']['L'] = 28; IBAN['A']['D'] = 24; IBAN['A']['T'] = 20;
+  IBAN['A']['Z'] = 28; IBAN['B']['H'] = 22; IBAN['B']['E'] = 16;
+  IBAN['B']['A'] = 20; IBAN['B']['R'] = 29; IBAN['B']['G'] = 22;
+  IBAN['C']['R'] = 21; IBAN['H']['R'] = 21; IBAN['C']['Y'] = 28;
+  IBAN['C']['Z'] = 24; IBAN['D']['K'] = 18; IBAN['D']['O'] = 28;
+  IBAN['E']['E'] = 20; IBAN['F']['O'] = 18; IBAN['F']['I'] = 18;
+  IBAN['F']['R'] = 27; IBAN['G']['E'] = 22; IBAN['D']['E'] = 22;
+  IBAN['G']['I'] = 23; IBAN['G']['R'] = 27; IBAN['G']['L'] = 18;
+  IBAN['G']['T'] = 28; IBAN['H']['U'] = 28; IBAN['I']['S'] = 26;
+  IBAN['I']['E'] = 22; IBAN['I']['L'] = 23; IBAN['I']['T'] = 27;
+  IBAN['K']['Z'] = 20; IBAN['K']['W'] = 30; IBAN['L']['V'] = 21;
+  IBAN['L']['B'] = 28; IBAN['L']['I'] = 21; IBAN['L']['T'] = 20;
+  IBAN['L']['U'] = 20; IBAN['M']['K'] = 19; IBAN['M']['T'] = 31;
+  IBAN['M']['R'] = 27; IBAN['M']['U'] = 30; IBAN['M']['D'] = 24;
+  IBAN['M']['C'] = 27; IBAN['M']['E'] = 22; IBAN['N']['L'] = 18;
+  IBAN['N']['O'] = 15; IBAN['P']['K'] = 24; IBAN['P']['S'] = 29;
+  IBAN['P']['L'] = 28; IBAN['P']['T'] = 25; IBAN['R']['O'] = 24;
+  IBAN['S']['M'] = 27; IBAN['S']['A'] = 24; IBAN['R']['S'] = 22;
+  IBAN['S']['K'] = 24; IBAN['S']['I'] = 19; IBAN['E']['S'] = 24;
+  IBAN['S']['E'] = 24; IBAN['C']['H'] = 21; IBAN['T']['N'] = 24;
+  IBAN['T']['R'] = 26; IBAN['A']['E'] = 23; IBAN['G']['B'] = 22;
+  IBAN['V']['G'] = 24;
+
   char buf[input_len + 1];
   int argi, append, len;
 
